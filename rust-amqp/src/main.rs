acd5ba08187ef608edb1565b25fe53e3dfbfa786
Máron <maron.franca@gmail.com>
2021-02-24 21:15:07 -0300

    patter queue file

--- rust-amqp/src/main.rs
+++ rust-amqp/src/main.rs
@@ -1,9 +1,7 @@
-use amiquip::ConsumerOptions;
-use amiquip::FieldTable;
-use amiquip::QueueDeclareOptions;
-use amiquip::{Connection, ConsumerMessage};
+use amiquip::Connection;
 
 mod dtos;
+mod pattern_queue;
 mod patterns;
 
 const URL: &str = "amqp://guest:guest@localhost:5672";
@@ -13,46 +11,8 @@ fn main() {
         Ok(conn) => conn,
         Err(error) => panic!("Connection error: {:?}", error),
     };
-    let channel = connection.open_channel(None).unwrap();
-    let queue = channel
-        .queue_declare(
-            "q_exam_pattern",
-            QueueDeclareOptions {
-                durable: true,
-                exclusive: false,
-                auto_delete: false,
-                arguments: FieldTable::default(),
-            },
-        )
-        .unwrap();
-    let consumer = queue
-        .consume(ConsumerOptions {
-            exclusive: true,
-            no_ack: true,
-            ..ConsumerOptions::default()
-        })
-        .unwrap();
 
-    for message in consumer.receiver().iter() {
-        match message {
-            ConsumerMessage::Delivery(delivery) => {
-                let body = String::from_utf8_lossy(&delivery.body);
-                let start_exam_data: dtos::pattern_dto::PatternDto =
-                    serde_json::from_str(&body).unwrap();
-                if start_exam_data.pattern == "start_exam" {
-                    patterns::create_queue::create_queue(&mut connection, body);
-                } else if start_exam_data.pattern == "answer_question" {
-                    patterns::answer_question::answer_question(&mut connection, body);
-                } else if start_exam_data.pattern == "finish_exam" {
-                    patterns::finish_exam::finish_exam(body);
-                }
-            }
-            other => {
-                println!("Consumer ended: {:?}", other);
-                break;
-            }
-        }
-    }
+    pattern_queue::pattern_queue(&mut connection);
 
     connection.close().unwrap()
 }
---
+++ rust-amqp/src/pattern_queue.rs
@@ -0,0 +1,46 @@
+use amiquip::{Connection, ConsumerMessage, ConsumerOptions, FieldTable, QueueDeclareOptions};
+
+use crate::{dtos, patterns};
+pub fn pattern_queue(connection: &mut Connection) {
+    let channel = connection.open_channel(None).unwrap();
+    let queue = channel
+        .queue_declare(
+            "q_exam_pattern",
+            QueueDeclareOptions {
+                durable: true,
+                exclusive: false,
+                auto_delete: false,
+                arguments: FieldTable::default(),
+            },
+        )
+        .unwrap();
+    let consumer = queue
+        .consume(ConsumerOptions {
+            exclusive: true,
+            no_ack: true,
+            ..ConsumerOptions::default()
+        })
+        .unwrap();
+
+    for message in consumer.receiver().iter() {
+        match message {
+            ConsumerMessage::Delivery(delivery) => {
+                let body = String::from_utf8_lossy(&delivery.body);
+                let start_exam_data: dtos::pattern_dto::PatternDto =
+                    serde_json::from_str(&body).unwrap();
+                println!("{:#?}", start_exam_data);
+                if start_exam_data.pattern == "start_exam" {
+                    patterns::create_queue::create_queue(connection, body, pool);
+                } else if start_exam_data.pattern == "answer_question" {
+                    patterns::answer_question::answer_question(connection, body, pool);
+                } else if start_exam_data.pattern == "finish_exam" {
+                    patterns::finish_exam::finish_exam(body, pool);
+                }
+            }
+            other => {
+                println!("Consumer ended: {:?}", other);
+                break;
+            }
+        }
+    }
+}
