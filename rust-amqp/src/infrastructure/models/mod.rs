8087cdd54e9ffb08118bd657ef1fa8877a2851a1
Máron <maron.franca@gmail.com>
2021-03-07 10:19:57 -0300

    models mod

--- rust-amqp/src/infrastructure/models/mod.rs
+++ rust-amqp/src/infrastructure/models/mod.rs
@@ -1,2 +1,30 @@
-pub mod exam_model;
-pub use exam_model::{AnswerCorretionModel, AnswerModel, ExamModel, IdModel, QuestionModel};
+use sqlx::FromRow;
+
+#[derive(FromRow, Debug)]
+pub struct ExamModel {
+    pub id: i32,
+    pub name: String,
+    pub description: String,
+}
+
+#[derive(FromRow, Debug)]
+pub struct QuestionModel {
+    pub id: i32,
+    pub content: String,
+}
+
+#[derive(FromRow, Debug)]
+pub struct AnswerModel {
+    pub id: i32,
+    pub content: String,
+}
+
+#[derive(FromRow, Debug)]
+pub struct AnswerCorretionModel {
+    pub is_correct: bool,
+}
+
+#[derive(FromRow, Debug)]
+pub struct IdModel {
+    pub id: i32,
+}
